6c9877714864c4e4384b2db86334ffa2fc9288f0
Sebastian Sdorra <s.sdorra@gmail.com>
2016-03-20 19:05:54 +0100

    added favions and logo

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,69 @@ var del = require('del');
 var browserSync = require('browser-sync').create();
 var $ = require('gulp-load-plugins')();
 
+gulp.task('favicon-ico', function(){
+	return gulp.src('src/favicon.ico')
+						 .pipe(gulp.dest('./dist/images/favicon'));
+});
+
+gulp.task('favicon-png', function(){
+	var resizecfg = [{
+		width: 64,
+		rename: {
+			suffix: '-64px'
+		}
+	},{
+	  width: 32,
+		rename: {
+			suffix: '-32px'
+		}
+	},{
+		width: 16,
+		rename: {
+			suffix: '-16px'
+		}
+	}];
+
+	return gulp.src('src/images/favicon/*')
+						 .pipe($.responsive({
+							 '*.png': resizecfg
+						 }))
+						 .pipe(gulp.dest('./dist/images/favicon'));
+});
+
+gulp.task('favicon', ['favicon-ico', 'favicon-png']);
+
+gulp.task('logo', function(){
+	var resizecfg = [{
+		width: 640,
+		rename: {
+			suffix: '-640px'
+		}
+	},{
+	  width: 320,
+		rename: {
+			suffix: '-320px'
+		}
+	},{
+		width: 160,
+		rename: {
+			suffix: '-160px'
+		}
+	},{
+		width: 30,
+		quality: 100,
+		rename: {
+			suffix: '-30px'
+		}
+	}];
+
+	return gulp.src('src/images/logo/*')
+						 .pipe($.responsive({
+							 '*.png': resizecfg
+						 }))
+						 .pipe(gulp.dest('./dist/images/logo'));
+});
+
 gulp.task('fonts', function(){
 	return gulp.src('bower_components/bootstrap-sass/assets/fonts/**')
 						 .pipe(gulp.dest('./dist/fonts'));
@@ -19,9 +82,9 @@ gulp.task('scss', function(){
 						 .pipe(gulp.dest('./dist/css'));
 });
 
-gulp.task('default', ['scss', 'fonts']);
+gulp.task('default', ['scss', 'fonts', 'logo', 'favicon']);
 
-gulp.task('serve', ['scss', 'fonts'], function(){
+gulp.task('serve', ['scss', 'logo', 'favicon'], function(){
 	browserSync.init(['dist', 'src/**/*.html', 'src/**/*.css', 'src/**/*.js'], {
 		server: {
 			baseDir: ["src", "dist", "bower_components"]
